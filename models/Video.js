a278f556621e2bb43d97bb84eb34b68c56a09b29
codedvm <107950826+codedvm@users.noreply.github.com>
2025-07-20 14:17:33 +0100

    Add files via upload

--- models/Video.js
+++ models/Video.js
@@ -1,9 +1,7 @@
 
 const mongoose = require('mongoose');
-
 const videoSchema = new mongoose.Schema({
     title: { type: String, required: true },
     url: { type: String, required: true },
 });
-
 module.exports = mongoose.model('Video', videoSchema);
